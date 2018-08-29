b1ba42c2fbfdd10e5e10bb430dcb588ad402c306
anne-glerum <a.c.glerum@uu.nl>
2018-08-29 15:11:44 +0200

    Make particles work with free surface

--- source/simulator/core.cc
+++ source/simulator/core.cc
@@ -28,6 +28,7 @@
 
 #include <aspect/simulator/assemblers/interface.h>
 #include <aspect/geometry_model/initial_topography_model/zero_topography.h>
+#include <aspect/postprocess/particles.h>
 
 #include <deal.II/base/index_set.h>
 #include <deal.II/base/conditional_ostream.h>
@@ -1109,7 +1110,32 @@ namespace aspect
     // surface active, since the mapping must be in place before applying boundary
     // conditions that rely on it (such as no flux BCs).
     if (parameters.free_surface_enabled)
+    {
+      // if particles are used, the ParticleHandler will have a pointer to the Mapping
+      // Mapping is reset by the free surface, so we need the ParticleHandler to release
+      // the Mapping object reference. After the free surface has updated the mapping,
+      // we set the pointer again.
+      // TODO dont get the ParticleHandler twice
+      const bool use_particles = postprocess_manager.template has_matching_postprocessor<Postprocess::Particles<dim> >();
+      // reset ParticleHandler's pointer to mapping
+      if(use_particles)
+      {
+        Postprocess::Particles<dim> &particle_postprocessor =
+           const_cast<Postprocess::Particles<dim> &>(postprocess_manager.template get_matching_postprocessor<Postprocess::Particles<dim> >());
+        Particle::ParticleHandler<dim> &particle_handler = const_cast<Particle::ParticleHandler<dim> &> (particle_postprocessor.get_particle_world().get_particle_handler());
+        particle_handler.clear_particle_handler_mapping();
+      }
       free_surface->setup_dofs();
+
+      // reset ParticleHandler's pointer to the current mapping
+      if(use_particles)
+      {
+        Postprocess::Particles<dim> &particle_postprocessor =
+            const_cast<Postprocess::Particles<dim> &>(postprocess_manager.template get_matching_postprocessor<Postprocess::Particles<dim> >());
+        Particle::ParticleHandler<dim> &particle_handler = const_cast<Particle::ParticleHandler<dim> &> (particle_postprocessor.get_particle_world().get_particle_handler());
+        particle_handler.set_particle_handler_mapping(*mapping);
+      }
+    }
 
 
     // reinit the constraints matrix and make hanging node constraints
@@ -1694,7 +1720,18 @@ namespace aspect
 
         // see if we want to terminate
         if (termination.first)
+        {
+          const bool use_particles = postprocess_manager.template has_matching_postprocessor<Postprocess::Particles<dim> >();
+          // reset ParticleHandler's pointer to mapping
+          if(use_particles)
+          {
+            Postprocess::Particles<dim> &particle_postprocessor =
+               const_cast<Postprocess::Particles<dim> &>(postprocess_manager.template get_matching_postprocessor<Postprocess::Particles<dim> >());
+            Particle::ParticleHandler<dim> &particle_handler = const_cast<Particle::ParticleHandler<dim> &> (particle_postprocessor.get_particle_world().get_particle_handler());
+            particle_handler.clear_particle_handler_mapping();
+          }
           break;
+        }
       }
     while (true);
 
