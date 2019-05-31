b241e7abcb6e30d3c12785df9840424916aaddc0
anne-glerum <a.c.glerum@uu.nl>
2019-05-30 19:12:55 -0600

    Iterate over boundary ids

--- source/mesh_deformation/function.cc
+++ source/mesh_deformation/function.cc
@@ -59,10 +59,13 @@ namespace aspect
     void
     BoundaryFunction<dim>::compute_velocity_constraints_on_boundary(const DoFHandler<dim> &mesh_deformation_dof_handler,
                                                                     ConstraintMatrix &mesh_velocity_constraints,
-                                                                    std::set<types::boundary_id> boundary_id) const
+                                                                    std::set<types::boundary_id> boundary_ids) const
     {
+      // Loop over all boundary indicators to set the velocity constraints
+      for (std::set<types::boundary_id>::const_iterator boundary_id = boundary_ids.begin();
+          boundary_id != boundary_ids.end(); ++boundary_id)
       VectorTools::interpolate_boundary_values (mesh_deformation_dof_handler,
-                                                *boundary_id.begin(),
+                                                *boundary_id,
                                                 function,
                                                 mesh_velocity_constraints);
     }
