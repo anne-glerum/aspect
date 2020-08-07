01edeceb5739d520af5618c514e2625847acd111
Esther Heckenbach <hecken@gfz-potsdam.de>
2020-08-07 15:17:13 +0200

    added friction parameters in enter subsection of visco_plastic but got an error

--- source/material_model/visco_plastic.cc
+++ source/material_model/visco_plastic.cc
@@ -797,6 +797,8 @@ namespace aspect
           EquationOfState::MulticomponentIncompressible<dim>::declare_parameters (prm);
 
           Rheology::StrainDependent<dim>::declare_parameters (prm);
+          
+          Rheology::FrictionOptions<dim>::declare_parameters (prm);
 
           Rheology::FrictionOptions<dim>::declare_parameters (prm);
 
