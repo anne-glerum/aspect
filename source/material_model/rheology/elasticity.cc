01aadaf18c75ee46d64ad906d869e1d734c8eec6
Esther Heckenbach <hecken@gfz-potsdam.de>
2020-09-07 13:38:02 +0200

    included Daniels changes for elastic timestep handling

--- source/material_model/rheology/elasticity.cc
+++ source/material_model/rheology/elasticity.cc
@@ -96,6 +96,12 @@ namespace aspect
                            Patterns::Bool (),
                            "Whether to apply a stress averaging scheme to account for differences "
                            "between the fixed elastic time step and numerical time step. ");
+        prm.declare_entry ("Stabilization time scale factor", "1.",
+                           Patterns::Double (1.),
+                           "A stabilization factor for the elastic stresses that influence how fast "
+                           "elastic stresses adjust to deformation. 1.0 is equivalent to no stabilization, "
+                           "and infinity is equivalent to not applying elastic stresses at all. The "
+                           "factor is multiplied with the computational time step to create a time scale.");
       }
 
 
@@ -122,6 +128,8 @@ namespace aspect
         if (use_stress_averaging)
           AssertThrow(use_fixed_elastic_time_step == true,
                       ExcMessage("Stress averaging can only be used if 'Use fixed elastic time step' is set to true'"));
+
+        stabilization_time_scale_factor = prm.get_double ("Stabilization time scale factor");
 
         fixed_elastic_time_step = prm.get_double ("Fixed elastic time step");
         AssertThrow(fixed_elastic_time_step > 0,
@@ -335,7 +343,7 @@ namespace aspect
                                this->simulator_is_past_initialization() &&
                                use_fixed_elastic_time_step == false )
                              ?
-                             this->get_timestep()
+                             this->get_timestep() * stabilization_time_scale_factor
                              :
                              fixed_elastic_time_step);
         return dte;
