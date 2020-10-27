fb2db207b88b4b5d0b2a8b03093b2ef31dcd37b4
Esther Heckenbach <hecken@gfz-potsdam.de>
2020-10-27 15:15:45 +0100

    put the max() into an if

--- source/time_stepping/interface.cc
+++ source/time_stepping/interface.cc
@@ -147,11 +147,11 @@ namespace aspect
 
       // take either the old timestep size times the cutback amount or the previously determined
       // timestep size if this is smaller. Prevents recalculating and recalculating with continuously
-      // decreasing timestep sizes and makes sure the minimum time step size is taken into account
-      // also for the repeated timestep length.
+      // decreasing timestep sizes
       repeat_step_size = std::min(repeat_step_size, info.next_time_step_size);
-      // make sure to consider the minimum time step size
-      repeat_step_size = std::max(repeat_step_size, minimum_time_step_size);
+      // make sure to consider the minimum time step size also for the repeated timestep length.
+      if (info.reduced_by_termination_plugin = false):
+          repeat_step_size = std::max(repeat_step_size, minimum_time_step_size);
 
 
       reaction = static_cast<Reaction>(Utilities::MPI::min(static_cast<int>(reaction), this->get_mpi_communicator()));
@@ -169,7 +169,7 @@ namespace aspect
     template <int dim>
     double
     Manager<dim>::get_next_time_step_size() const
-    {
+  {
       return next_time_step_size;
     }
 
