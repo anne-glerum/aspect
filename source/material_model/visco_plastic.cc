7a9437d06892ab7cf76b6f9b5f2999352a681ee4
EstherHeck <hecken@gfz-potsdam.de>
2021-03-19 09:27:33 +0100

    uncomment min healing time step to see if Lapusta only does not work bc of theta

--- source/material_model/visco_plastic.cc
+++ source/material_model/visco_plastic.cc
@@ -498,6 +498,7 @@ namespace aspect
     ViscoPlastic<dim>::
     compute_min_healing_time_step (const std::vector<double> &composition) const
     {
+      const double min_healing_time_step = std::numeric_limits<double>::max(); /*
       double min_healing_time_step = 0.2 * composition[rheology->friction_options.theta_composition_index];
 //std::cout << " composition[rheology->friction_options.theta_composition_index]: "<<composition[rheology->friction_options.theta_composition_index]<<std::endl;
       AssertThrow((std::isinf( min_healing_time_step) || numbers::is_nan( min_healing_time_step)) == false, ExcMessage(
@@ -506,7 +507,7 @@ namespace aspect
                     "rate-and-state like friction, don't forget to check on a,b, and the critical slip distance, or theta."));
       // ToDo: this time step somehow becomes negative, because theta still becomes negative. So in this case I make it very large so it does no harm
       if (min_healing_time_step <= 0)
-        min_healing_time_step =  std::numeric_limits<double>::max();
+        min_healing_time_step =  std::numeric_limits<double>::max();*/
       return min_healing_time_step;
     }
 
