54ab35ece9ddb78864dcffb33e7be8d775e297d8
anne-glerum <a.c.glerum@uu.nl>
2022-08-02 16:47:33 +0200

    Add includes lost in rebase

--- source/postprocess/visualization/stress_second_invariant.cc
+++ source/postprocess/visualization/stress_second_invariant.cc
@@ -22,7 +22,9 @@
 
 #include <aspect/postprocess/visualization/stress_second_invariant.h>
 
-
+#include <aspect/material_model/rheology/elasticity.h>
+#include <aspect/material_model/visco_plastic.h>
+#include <aspect/material_model/viscoelastic.h>
 
 namespace aspect
 {
