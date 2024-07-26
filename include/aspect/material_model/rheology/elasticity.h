961b1f5f35df0c36c666269945116024e0ea908e
Bob Myhill <myhill.bob@gmail.com>
2024-07-26 11:38:19 +0100

    added elasticity to CompositeViscoPlastic

--- include/aspect/material_model/rheology/elasticity.h
+++ include/aspect/material_model/rheology/elasticity.h
@@ -154,6 +154,12 @@ namespace aspect
           get_elastic_shear_moduli () const;
 
           /**
+           * Return the values of the damper viscosity used in the rheology model.
+           */
+          double
+          get_damper_viscosity() const;
+
+          /**
            * Calculate the effective elastic viscosity (this is the equivalent viscosity of
            * a material which was unstressed at the end of the previous timestep).
            */
